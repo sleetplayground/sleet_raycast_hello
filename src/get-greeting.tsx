0245035fa7d5fca637af3d9451e759cb35933c96
The SunShining <thesunshiningmin@gmail.com>
2025-04-03 11:18:21 -0500

    Update get-greeting.tsx

--- src/get-greeting.tsx
+++ src/get-greeting.tsx
@@ -14,10 +14,13 @@ const GetGreeting = () => {
   const [contractName, setContractName] = useState<string>("");
   const [networkId, setNetworkId] = useState<string>("");
 
+  const [nodeUrl, setNodeUrl] = useState<string>("");
+
   const refreshGreeting = async () => {
     const { nodeUrl, contractName, networkId } = await getNetworkConfig();
     setContractName(contractName);
     setNetworkId(networkId);
+    setNodeUrl(nodeUrl);
 
     const near = await connect({ nodeUrl, networkId });
     const account = await near.account(contractName);
@@ -47,6 +50,7 @@ ${greeting}
 ## Details
 - **Contract Name:** ${contractName}
 - **Network:** ${networkId}
+- **RPC Endpoint:** ${nodeUrl}
 
 ---
 *Click the refresh button to update the greeting*
